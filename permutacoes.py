b25497a25dd99dc80da2952fbee07f4c36495a83
lui5085 <121206535+lui5085@users.noreply.github.com>
2023-09-05 16:06:49 -0300

    Update permutacoes.py

--- permutacoes.py
+++ permutacoes.py
@@ -9,16 +9,16 @@ def ordenacao(lista, n):
     return lista
 l1 = [1, 3, 3, 4, 5]
 l2 = [4, 3, 0, 5, 8]
-if len(l1) != len(l2) or l1 == l2:
+if len(l1) != len(l2) or l1 == l2: #casos em que as listas não tem tamanhos iguais ou que tem são iguais, não são considerados permutações
     print("As listas não são permutações")
 else:
     l1 = ordenacao(l1, len(l1))
     l2 = ordenacao(l2, len(l2))
     contador = 0
-    for i in range(len(l1)):
+    for i in range(len(l1)): #comparação de cada casa da lista
         if l1[i] == l2[i]:
             contador += 1
-    if contador == len(l1):
+    if contador == len(l1): #listas que são permutações, quando ordenadas vão ser iguais, logo se o contador tiver o valor do tamanho de alguma das listas então elas serão permutações
         print("As listas são permutações")
     else:
         print("As listas não são permutações")
